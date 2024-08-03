067444864e155352782fedf00747aa9eacdff8c0
Vlada Bessikalo <bess.vlada@gmail.com>
2024-08-03 13:34:17 +0200

    adjusted html, css, javascript

--- js/index.js
+++ js/index.js
@@ -1,295 +1,284 @@
-/* Navigation and burger */
-const hamburgerMenu = document.querySelector('.nav__hamburger');
-const menu = document.getElementById('nav-menu');
-const bannerLinkBtn = document.querySelector('.banner__link-btn');
+document.addEventListener('DOMContentLoaded', () => {
+    /* Navigation and burger */
+    const hamburgerMenu = document.querySelector('.nav__hamburger');
+    const menu = document.getElementById('nav-menu');
 
-function toggleMenu() {
-    menu.classList.toggle('active');
-    hamburgerMenu.classList.toggle('active');
-    document.body.classList.toggle('no-scroll', menu.classList.contains('active'));
-}
-
-function scrollToSection(event) {
-    event.preventDefault();
-    const targetId = event.target.getAttribute('href'); 
-    const targetElement = document.querySelector(targetId);
-
-    targetElement.scrollIntoView({
-        behavior: 'smooth'
-    });
-
-    if (window.innerWidth <= 768 && menu.classList.contains('active')) {
-        toggleMenu();
+    function toggleMenu() {
+        menu.classList.toggle('active');
+        hamburgerMenu.classList.toggle('active');
+        document.body.classList.toggle('no-scroll', menu.classList.contains('active'));
     }
-}
 
-function navigation() {
-    const links = document.querySelectorAll('.nav__menu-link');
-    links.forEach(link => {
-        link.addEventListener('click', scrollToSection);
-    });
-}
+    function scrollToSection(event) {
+        event.preventDefault();
+        const targetId = event.target.getAttribute('href') || '#pets'; 
+        const targetElement = document.querySelector(targetId);
 
-hamburgerMenu.addEventListener('click', toggleMenu);
+        targetElement.scrollIntoView({
+            behavior: 'smooth'
+        });
 
-bannerLinkBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    const targetElement = document.querySelector('#pets');
-    targetElement.scrollIntoView({
-        behavior: 'smooth'
-    });
-});
-
-navigation();
-
-/* Slider */
-function initializeSlider(sliderContainer) {
-    const slides = sliderContainer.querySelector('.carousel');
-    const slideItems = sliderContainer.querySelectorAll('.carousel__item');
-    const nextButton = sliderContainer.querySelector('.slider-container__btn--next');
-    const prevButton = sliderContainer.querySelector('.slider-container__btn--prev');
-
-    let currentIndex = 0;
-    const totalSlides = slideItems.length;
-    const visibleSlides = 4;
-    let slideWidth;
-
-    function cloneSlides() {
-        const slidesArray = Array.from(slideItems);
-        for (let i = 0; i < visibleSlides; i++) {
-            let cloneFirst = slidesArray[i].cloneNode(true);
-            let cloneLast = slidesArray[totalSlides - 1 - i].cloneNode(true);
-            slides.appendChild(cloneFirst);
-            slides.prepend(cloneLast);
+        if (window.innerWidth <= 768 && menu.classList.contains('active')) {
+            toggleMenu();
         }
     }
 
-    function initSlider() {
-        cloneSlides();
-        slideWidth = slideItems[0].clientWidth;
-        slides.style.transform = `translateX(-${visibleSlides * slideWidth}px)`;
-        slides.addEventListener('transitionend', handleTransitionEnd);
-        window.addEventListener('resize', updateSlider);
+    function navigation() {
+        const links = document.querySelectorAll('.nav__menu-link, .banner__link-btn');
+        links.forEach(link => {
+            link.addEventListener('click', scrollToSection);
+        });
     }
 
-    function updateSlider() {
-        slideWidth = slideItems[0].clientWidth;
-        slides.style.transition = 'none'; 
-        slides.style.transform = `translateX(-${(currentIndex + visibleSlides) * slideWidth}px)`;
-        slides.offsetHeight; 
-        slides.style.transition = ''; 
-    }
+    hamburgerMenu.addEventListener('click', toggleMenu);
 
-    function nextSlide() {
-        currentIndex++;
-        const newTransform = -((currentIndex + visibleSlides) * slideWidth);
-        slides.style.transform = `translateX(${newTransform}px)`;
-    }
+    navigation();
 
-    function prevSlide() {
-        currentIndex--;
-        const newTransform = -((currentIndex + visibleSlides) * slideWidth);
-        slides.style.transform = `translateX(${newTransform}px)`;
-    }
+    /* Slider */
+    function initializeSlider(sliderContainer) {
+        const slides = sliderContainer.querySelector('.carousel');
+        const slideItems = sliderContainer.querySelectorAll('.carousel__item');
+        const nextButton = sliderContainer.querySelector('.slider-container__btn--next');
+        const prevButton = sliderContainer.querySelector('.slider-container__btn--prev');
 
-    function handleTransitionEnd() {
-        if (currentIndex >= totalSlides) {
-            slides.style.transition = 'none'; 
-            currentIndex = 0;
+        let currentIndex = 0;
+        const totalSlides = slideItems.length;
+        const visibleSlides = 4;
+        let slideWidth;
+
+        function cloneSlides() {
+            const slidesArray = Array.from(slideItems);
+            for (let i = 0; i < visibleSlides; i++) {
+                let cloneFirst = slidesArray[i].cloneNode(true);
+                let cloneLast = slidesArray[totalSlides - 1 - i].cloneNode(true);
+                slides.appendChild(cloneFirst);
+                slides.prepend(cloneLast);
+            }
+        }
+
+        function initSlider() {
+            cloneSlides();
+            slideWidth = slideItems[0].clientWidth;
             slides.style.transform = `translateX(-${visibleSlides * slideWidth}px)`;
-            slides.offsetHeight; 
-            slides.style.transition = ''; 
-        } else if (currentIndex < 0) {
+            slides.addEventListener('transitionend', handleTransitionEnd);
+            window.addEventListener('resize', updateSlider);
+        }
+
+        function updateSlider() {
+            slideWidth = slideItems[0].clientWidth;
             slides.style.transition = 'none'; 
-            currentIndex = totalSlides - 1;
             slides.style.transform = `translateX(-${(currentIndex + visibleSlides) * slideWidth}px)`;
             slides.offsetHeight; 
-            slides.style.transition = '';
+            slides.style.transition = ''; 
         }
+
+        function nextSlide() {
+            currentIndex++;
+            const newTransform = -((currentIndex + visibleSlides) * slideWidth);
+            slides.style.transform = `translateX(${newTransform}px)`;
+        }
+
+        function prevSlide() {
+            currentIndex--;
+            const newTransform = -((currentIndex + visibleSlides) * slideWidth);
+            slides.style.transform = `translateX(${newTransform}px)`;
+        }
+
+        function handleTransitionEnd() {
+            if (currentIndex >= totalSlides) {
+                slides.style.transition = 'none'; 
+                currentIndex = 0;
+                slides.style.transform = `translateX(-${visibleSlides * slideWidth}px)`;
+                slides.offsetHeight; 
+                slides.style.transition = ''; 
+            } else if (currentIndex < 0) {
+                slides.style.transition = 'none'; 
+                currentIndex = totalSlides - 1;
+                slides.style.transform = `translateX(-${(currentIndex + visibleSlides) * slideWidth}px)`;
+                slides.offsetHeight; 
+                slides.style.transition = '';
+            }
+        }
+
+        initSlider();
+
+        nextButton.addEventListener('click', nextSlide);
+        prevButton.addEventListener('click', prevSlide);
     }
 
-    initSlider();
-
-    nextButton.addEventListener('click', nextSlide);
-    prevButton.addEventListener('click', prevSlide);
-}
-
-document.querySelectorAll('.slider-container').forEach(initializeSlider);
+    document.querySelectorAll('.slider-container').forEach(initializeSlider);
 
 
-/* Modal window */
-const modal = document.querySelector('.modal');
-const backdrop = document.querySelector('.backdrop');
-const userInputs = modal.querySelectorAll('input');
-const textArea = modal.querySelector('textarea');
-const cancelModalBtn = modal.querySelector('.button--cancel');
-const closeModalBtn = modal.querySelector('.button--close');
-const form = document.querySelector('.form');
-const modalTitle = document.querySelector('.modal__title');
-const adoptMeBtns = document.querySelectorAll('.button--adopt');
-const fosterBtn = document.querySelector('.button--foster');
-const volunteerBtn = document.querySelector('.button--volunteer');
-const successMessage = document.querySelector('.success-message');
-const successMessageBtn = document.querySelector('.success-message__btn');
+    /* Modal window */
+    const modal = document.querySelector('.modal');
+    const backdrop = document.querySelector('.backdrop');
+    const userInputs = modal.querySelectorAll('input');
+    const textArea = modal.querySelector('textarea');
+    const cancelModalBtn = modal.querySelector('.button--cancel');
+    const closeModalBtn = modal.querySelector('.button--close');
+    const submitBtn = document.querySelector('.button--submit');
+    const modalTitle = document.querySelector('.modal__title');
+    const adoptMeBtns = document.querySelectorAll('.button--adopt');
+    const fosterBtn = document.querySelector('.button--foster');
+    const volunteerBtn = document.querySelector('.button--volunteer');
+    const successMessage = document.querySelector('.success-message');
+    const successMessageBtn = document.querySelector('.success-message__btn');
 
 
-const toggleBackdrop = (show) => {
-    backdrop.classList.toggle('visible', show);
-    document.body.classList.toggle('no-scroll', show);
-};
-
-const showModal = (title) => {
-    modalTitle.textContent = title;
-    modal.style.display = 'flex';
-    toggleBackdrop(true);
-}
-
-const showSuccessMessage = () => {
-    successMessage.style.display = 'flex';
-    toggleBackdrop(true);
-}
-
-const closeSuccessMessage = () => {
-    successMessage.style.display = 'none';
-    toggleBackdrop(false);
-}
-
-successMessageBtn.addEventListener('click', closeSuccessMessage);
-
-
-const backdropClickHandler = () => {
-    closeModal();
-    clearInputs(); 
-    closeSuccessMessage()
-};
-
-const closeModal = () => {
-    modal.style.display = 'none';
-    toggleBackdrop(false);
-    clearInputs();
-};
-
-const clearInputs = () => {
-    for (const input of userInputs) {
-        input.value = '';
-        input.style.border = '1px solid #575757'
-    }
-    textArea.value = '';
-};
-
-adoptMeBtns.forEach(button => {
-    button.addEventListener('click', () => showModal('Adopt Form'));
-});
-
-fosterBtn.addEventListener('click', () => showModal('Foster Form'));
-
-volunteerBtn.addEventListener('click', () => showModal('Volunteer Form'));
-
-backdrop.addEventListener('click', backdropClickHandler);
-cancelModalBtn.addEventListener('click', closeModal);
-closeModalBtn.addEventListener('click', closeModal);
-userInputs.forEach(input => {
-    input.addEventListener('input', function() {
-      if (this.checkValidity()) {
-        this.style.border = '2px solid #fedc5a';
-      } else {
-        this.style.border = '2px solid red';
-      }
-    });
-});
-
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
-    let valid = true;
-    let messages = [];
-
-    const name = document.getElementById('name');
-    const email = document.getElementById('email');
-    const age = document.getElementById('age');
-    const occupation = document.getElementById('occupation');
-    const message = document.getElementById('message');
-
-    if (name.value.trim().length < 2) {
-        valid = false;
-        messages.push('Name must be at least 2 characters long.');
-    }
-
-    if (!email.checkValidity()) {
-        valid = false;
-        messages.push('Please enter a valid email address.');
-    }
-
-    if (age.value < 18) {
-        valid = false;
-        messages.push('Age must be at least 18.');
-    }
-
-    if (occupation.value.trim().length === 0) {
-        valid = false;
-        messages.push('Occupation is required.');
-    }
-
-    if (message.value.trim().length === 0) {
-        valid = false;
-        messages.push('Additional information is required.');
-    }
-
-    if (!valid) {
-        event.preventDefault(); 
-        alert(messages.join('\n'));
-        return;
-    }
-
-    const formData = {
-        name: name.value,
-        email: email.value,
-        age: age.value,
-        occupation: occupation.value,
-        message: message.value
+    const toggleBackdrop = (show) => {
+        backdrop.classList.toggle('visible', show);
+        document.body.classList.toggle('no-scroll', show);
     };
 
-    fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify(formData),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    })
+    const showModal = (title) => {
+        modalTitle.textContent = title;
+        modal.style.display = 'flex';
+        toggleBackdrop(true);
+    }
 
-    .then(response => {
-        if (!response.ok) {
-            throw new Error('Some Errow');
-        }
-        return response.json();
-    })
-    .then(data => {
-        console.log('Success:', data);
+    const backdropClickHandler = () => {
         closeModal();
-        setTimeout(() => {
-            showSuccessMessage();
-        }, 0)
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-        alert('There was an error submitting the form.');
+        closeSuccessMessage();
+    };
+
+    const closeModal = () => {
+        modal.style.display = 'none';
+        toggleBackdrop(false);
+        clearInputs();
+    };
+
+    const clearInputs = () => {
+        for (const input of userInputs) {
+            input.value = '';
+            input.style.border = '1px solid #575757'
+        }
+        textArea.value = '';
+    };
+
+    adoptMeBtns.forEach(button => {
+        button.addEventListener('click', () => showModal("Adopter's Form"));
     });
-    
-});
+
+    fosterBtn.addEventListener('click', () => showModal("Foster Form"));
+
+    volunteerBtn.addEventListener('click', () => showModal("Volunteer Form"));
+
+    backdrop.addEventListener('click', backdropClickHandler);
+    cancelModalBtn.addEventListener('click', closeModal);
+    closeModalBtn.addEventListener('click', closeModal);
+    userInputs.forEach(input => {
+        input.addEventListener('input', function() {
+        if (this.checkValidity()) {
+            this.style.border = '2px solid #fedc5a';
+        } else {
+            this.style.border = '2px solid red';
+        }
+        });
+    });
+
+    /* There was one problem with Codepen - the standard behavior of the submission form did not work. I assume it was due to some security settings in Codepen.  To do this, I changed the default behavior of the submit form so that when you click on the "Submit" button, the form is submitted ( submitBtn.addEventListener('click', () =>  {}, insted of form.addEventListener('submit', () =>  {}.*/
+    submitBtn.addEventListener('click', () => {
+        let valid = true;
+        let messages = [];
+
+        const name = document.getElementById('name');
+        const email = document.getElementById('email');
+        const age = document.getElementById('age');
+        const occupation = document.getElementById('occupation');
+        const message = document.getElementById('message');
+
+        if (name.value.trim().length < 2) {
+            valid = false;
+            messages.push('Name must be at least 2 characters long.');
+        }
+
+        if (!email.checkValidity()) {
+            valid = false;
+            messages.push('Please enter a valid email address.');
+        }
+
+        if (age.value < 18) {
+            valid = false;
+            messages.push('Age must be at least 18.');
+        }
+
+        if (occupation.value.trim().length === 0) {
+            valid = false;
+            messages.push('Occupation is required.');
+        }
+
+        if (message.value.trim().length === 0) {
+            valid = false;
+            messages.push('Additional information is required.');
+        }
+
+        if (!valid) {
+            event.preventDefault(); 
+            alert(messages.join('\n'));
+            return;
+        }
+
+        const formData = {
+            name: name.value,
+            email: email.value,
+            age: age.value,
+            occupation: occupation.value,
+            message: message.value
+        };
+
+        fetch('https://jsonplaceholder.typicode.com/posts', {
+            method: 'POST',
+            body: JSON.stringify(formData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Some Error');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Success:', data);
+            closeModal();
+            setTimeout(() => {
+                showSuccessMessage();
+            }, 0)
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+            alert('There was an error submitting the form.');
+        });
+    });
+
+    const showSuccessMessage = () => {
+        successMessage.style.display = 'flex';
+        toggleBackdrop(true);
+    }
+
+    const closeSuccessMessage = () => {
+        successMessage.style.display = 'none';
+        toggleBackdrop(false);
+    }
+
+    successMessageBtn.addEventListener('click', closeSuccessMessage);
 
 
-/* Scroll Up */
-const scrollBtn = document.querySelector('.button--scroll');
-const banner = document.querySelector('.banner');
+    /* Scroll Up */
+    const scrollBtn = document.querySelector('.button--scroll');
+    const banner = document.querySelector('.banner');
 
-window.addEventListener("scroll", () => {
-    window.scrollY > banner.clientHeight
-      ? scrollBtn.classList.add("show-btn")
-      : scrollBtn.classList.remove("show-btn");
-  });
+    window.addEventListener("scroll", () => {
+        window.scrollY > banner.clientHeight
+        ? scrollBtn.classList.add("show-btn")
+        : scrollBtn.classList.remove("show-btn");
+    });
 
-scrollBtn.addEventListener("click", () => {
-    window.scrollTo({
-        top: 0,
-        behavior: "smooth" 
+    scrollBtn.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth" 
+        });
     });
 });
